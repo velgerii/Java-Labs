35eca57823244e8aae29177d0e2383f49ad10fda
velgerii <velgerii00@gmail.com>
2023-05-06 08:48:45 +0300

    synchronized thread

---
+++ 5/Laba5/src/Runner.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Runner implements Runnable
+{
+    ArrayList<Integer> map;
+    ArrayList<Thread> runners;
+    int number;
+    int obstacle;
+
+   public Runner(ArrayList<Integer> map, Integer number, ArrayList<Thread> runners){
+       this.map = map;
+       this.number = number;
+       this.runners = runners;
+   }
+
+    @Override
+    public void run() {
+        System.out.println("Бегун "+ number+ " начал забег");
+        for (int i = 0; i < map.size();i++) {
+            obstacle = map.get(i);
+
+            try {
+                Thread.sleep(obstacle*1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        synchronized (runners)
+        {
+            System.out.println("Бегун " + number+" закончил забег");
+        }
+
+    }
+}
